7a7ff792b91f6c412f22b10daad5eecd349fda35
Chris Troutner <chris.troutner@gmail.com>
2022-05-16 10:21:10 -0700

    Adding footer to Filecoin

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,25 @@ const IndexPage = (props) => {
       <NFTs wallet={wallet}/>
 
       <hr />
-      <p>
-        The code creating this page is open source. <a
-          href="https://github.com/christroutner/nft-browser"
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Check it out on GitHub
-        </a>
-      </p>
+      <ul>
+        <li>
+          The code creating this page is open source. <a
+            href="https://github.com/christroutner/nft-browser"
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Check it out on GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://bafybeif4e4tuigw6yfzh7omssgqwelckoyizkklqlww5orsr37thxyjc4u.ipfs.dweb.link/"
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            This page is regularly backed up onto the Filecoin blockchain.
+          </a>
+        </li>
+      </ul>
     </>
   )
 }
