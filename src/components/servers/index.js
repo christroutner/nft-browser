942fcb2984bf6c7b4e0878da1ad728dca1481758
Chris Troutner <chris.troutner@gmail.com>
2022-06-15 10:24:10 -0700

    Dynamically loading servers from a gist

--- src/components/servers/index.js
+++ src/components/servers/index.js
@@ -1,28 +1,16 @@
 /*
   This component contains a drop-down form that lets the user select from
   a range of Global Back End servers.
-
-  <label>Choose a car:{' '}
-    <select name="cars" id="cars">
-    <option value="volvo">Volvo</option>
-    <option value="saab">Saab</option>
-    <option value="mercedes">Mercedes</option>
-    <option value="audi">Audi</option>
-    </select>
-  </label>
-  <br />
 */
-
 
 // Global npm libraries
 import React from 'react'
-// import AsyncSelect from 'react-select/async'
 import Select from 'react-dropdown-select'
 
 // Local libraries
-// import NFTCard from './nft-card'
+const GistServers = require('../../services/gist-servers')
 
-const options = [
+const defaultOptions = [
   { value: 'https://free-bch.fullstack.cash', label: 'https://free-bch.fullstack.cash' },
   { value: 'https://bc01-ca-bch-consumer.fullstackcash.nl', label: 'https://bc01-ca-bch-consumer.fullstackcash.nl' },
   { value: 'https://pdx01-usa-bch-consumer.fullstackcash.nl', label: 'https://pdx01-usa-bch-consumer.fullstackcash.nl' },
@@ -37,20 +25,23 @@ class ServerSelect extends React.Component {
     // this.wallet = props.wallet
 
     this.state = {
-      // nftData: [],
+      options: defaultOptions
     }
   }
 
-  // async componentDidMount() {
-  //
-  // }
+  async componentDidMount() {
+    const servers = await this.getServers()
+    // console.log('Server list retrieved from GitHub Gist.')
 
-  // async getServers() {
-  //   console.log('hello world')
-  //   return 'a'
-  // }
+    this.setState({
+      options: servers
+    })
+  }
 
+  // This is called when the a new drop-down item is selected.
   selectServer(value) {
+    if(!value) return
+
     console.log(`Server ${value[0].value} was selected. Reloading page...`)
     if(typeof window !== 'undefined') {
       window.location.href = `/?restURL=${value[0].value}`
@@ -61,18 +52,28 @@ class ServerSelect extends React.Component {
   render() {
     return (
       <div>
-        <Select options={options} onChange={(values) => this.selectServer(values)} />
+        <Select options={this.state.options} onChange={(values) => this.selectServer(values)} />
         <p style={{textAlign: 'center'}}>Having trouble loading the NFTs? If NFTs don't load after a minute, then
         try selecting a different back-end server.</p>
       </div>
     )
   }
 
+  // Retrieve an array of server URLs from the GitHub Gist.
+  async getServers() {
+    const gistLib = new GistServers()
+    const gistServers = await gistLib.getServerList()
+
+    const serversAry = []
+
+    for(let i =0; i < gistServers.length; i++) {
+      serversAry.push({value: gistServers[i].url, label: gistServers[i].url})
+    }
+
+    return serversAry
+  }
+
 }
 
-const getServers = async (input) => {
-  console.log('hello world')
-  return 'a'
-}
 
 export default ServerSelect
